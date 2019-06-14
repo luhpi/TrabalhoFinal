b700b5a1e1a8f14683bbbf7edd587d3f95e218d5
Luiza <luiza.pinheiro@telessauders.ufrgs.br>
2019-06-14 15:44:36 -0300

    added mao functions

--- TrabalhoFinal/src/dao/DAO.java
+++ TrabalhoFinal/src/dao/DAO.java
@@ -11,14 +11,42 @@ public class DAO {
 	private Connection con = null;
 	
 	//Create
-	public String createDAO(int code, String name){
+	public String createUser(String name, String senha, boolean apoio){
 		con = ConMySQL.getConnectionMySQL();
-		String sql = "INSERT INTO usuarios VALUES (?, ?)";
+		String sql = "INSERT INTO usuarios VALUES (?, ?, ?)";
 		
 		try {
 			PreparedStatement prepS = con.prepareStatement(sql);
-			prepS.setInt(1, code);
-			prepS.setString(2, name);
+			prepS.setString(1, name);
+			prepS.setString(2, senha);
+            prepS.setBoolean(3, apoio);
+            prepS.executeUpdate();
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return name;
+	}
+
+
+	public String createSolicitante(String curso, String nome, String telefone, String email){
+		con = ConMySQL.getConnectionMySQL();
+		String sql = "INSERT INTO solicitante VALUES (?, ?, ?, ?)";
+		
+		try {
+			PreparedStatement prepS = con.prepareStatement(sql);
+			prepS.setString(1, curso);
+			prepS.setString(2, nome);
+            prepS.setString(3, telefone);
+            prepS.setString(4, email);
 			prepS.executeUpdate();
 			con.close();
 		} catch (SQLException e) {
@@ -34,19 +62,72 @@ public class DAO {
 		}
 		return name;
 	}
-	
-	//Read
-	public String readDAO(int code){
+
+
+	public String createAtividade(String atividade, String data_req_ini, String data_req_fin){
 		con = ConMySQL.getConnectionMySQL();
-		String sql = "SELECT * FROM usuarios WHERE id=?";
+		String sql = "INSERT INTO atividade VALUES (?, ?, ?)";
+		
+		try {
+			PreparedStatement prepS = con.prepareStatement(sql);
+			prepS.setString(1, atividade);
+			prepS.setString(2, data_req_ini);
+            prepS.setString(3, data_req_fin);
+			prepS.executeUpdate();
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return name;
+	}
+
+	public String createApoio(String sala, String servidor, String obs, String data_res_ini, String data_res_fin){
+		con = ConMySQL.getConnectionMySQL();
+		String sql = "INSERT INTO usuarios VALUES (?, ?, ?, ?)";
+		
+		try {
+			PreparedStatement prepS = con.prepareStatement(sql);
+			prepS.setString(1, servidor);
+			prepS.setString(2, obs);
+            prepS.setString(3, data_res_ini);
+            prepS.setString(4, data_res_fin);
+			prepS.executeUpdate();
+			con.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return name;
+	}
+
+	//Read
+	public String readUser(String nome, String senha){
+		con = ConMySQL.getConnectionMySQL();
+		String sql = "SELECT * FROM usuarios WHERE nome=? and senha=?";
 		
 		PreparedStatement prepS;
 		try {
 			prepS = con.prepareStatement(sql);
-			prepS.setInt(1, code);
+			prepS.setString(1, nome);
+            prepS.setString(2, senha);
 			ResultSet res = prepS.executeQuery();
 			while(res.next()){
-				name = res.getString(2);
+				name = res.getString(3);
 			}
 			con.close();
 		} catch (SQLException e) {
@@ -64,14 +145,14 @@ public class DAO {
 	}
 	
 	//Update
-	public void updateDAO(int code, String name){
+	public void updateUser(String name, String senha){
 		con = ConMySQL.getConnectionMySQL();
-		String sql = "UPDATE usuarios SET nome=? WHERE id=?";
+		String sql = "UPDATE usuarios SET senha=? WHERE nome=?";
 		
 		try {
 			PreparedStatement prepS = con.prepareStatement(sql);
-			prepS.setString(1, name);
-			prepS.setInt(2, code);
+			prepS.setString(1, senha);
+			prepS.setString(2, name);
 			prepS.executeUpdate();
 			con.close();
 		} catch (SQLException e) {
@@ -88,14 +169,15 @@ public class DAO {
 	}
 	
 	//Delete
-	public void deleteDAO(int code){
+	public void deleteUser(String nome, String senha){
 		con = ConMySQL.getConnectionMySQL();
-		String sql = "DELETE FROM usuarios WHERE id=?";
+		String sql = "DELETE FROM usuarios WHERE nome=? and senha=?";
 		
 		PreparedStatement prepS;
 		try {
 			prepS = con.prepareStatement(sql);
-			prepS.setInt(1, code);
+			prepS.setString(1, nome);
+            prepS.setString(2, senha);
 			prepS.executeUpdate();
 			con.close();
 		} catch (SQLException e) {
